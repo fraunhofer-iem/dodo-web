15f6828c438291974d077be0c2076ad8cc1954e0
janniclas <j.n.struewer@gmail.com>
2021-05-15 22:54:36 +0200

    intersection observer cleanup

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -2,17 +2,13 @@ import React, { useEffect, useRef } from "react";
 import Section from "../section/Section";
 
 const Content = () => {
-  const root = useRef<HTMLDivElement>(null);
   const observer = useRef<IntersectionObserver>();
   const about = React.createRef<HTMLElement>();
   const results = React.createRef<HTMLElement>();
   const team = React.createRef<HTMLElement>();
   const partners = React.createRef<HTMLElement>();
 
-  const callback = (
-    entries: IntersectionObserverEntry[],
-    observer: IntersectionObserver
-  ) => {
+  const callback = (entries: IntersectionObserverEntry[]) => {
     console.log("intersection detected");
     console.log(entries);
     // we can use the id of the element, which equals the href of the nav buttons,
@@ -23,13 +19,14 @@ const Content = () => {
   };
 
   useEffect(() => {
-    console.log(root.current);
     const options = {
       root: null,
       rootMargin: "0px",
       threshold: 1.0,
     };
+
     observer.current = new IntersectionObserver(callback, options);
+
     if (about.current !== null) observer.current.observe(about.current);
     if (results.current !== null) observer.current.observe(results.current);
     if (team.current !== null) observer.current.observe(team.current);
@@ -39,15 +36,15 @@ const Content = () => {
       console.log("disconnecting");
       observer.current?.disconnect();
     };
-  }, [observer, root, about, results, team, partners]);
+  }, [observer, about, results, team, partners]);
 
   return (
-    <div ref={root}>
+    <React.Fragment>
       <Section href={"about"} ref={about} title={"Hello World"}></Section>
       <Section href={"results"} ref={results} title={"Next"}></Section>
       <Section href={"team"} ref={team} title={"WhoopWhoop"}></Section>
       <Section href={"partners"} ref={partners} title={"WhoopWhoop"}></Section>
-    </div>
+    </React.Fragment>
   );
 };
 
