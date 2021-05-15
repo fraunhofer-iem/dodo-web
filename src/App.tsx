ae7457a87f182c1118b311cb433cf1613dd57ffc
janniclas <j.n.struewer@gmail.com>
2021-05-15 14:57:31 +0200

    many new ideas backup commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,78 @@
+import { orange } from "@material-ui/core/colors";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import React from "react";
 
 import NavBar from "./components/navbar/Navbar";
 import Overview from "./components/overview/Overview";
+import Section from "./components/section/Section";
+
+declare module "@material-ui/core/styles/createMuiTheme" {
+  interface Theme {
+    status: {
+      danger: string;
+    };
+  }
+  // allow configuration using `createMuiTheme`
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    };
+  }
+}
+
+const theme = createMuiTheme({
+  palette: {
+    text: {
+      primary: "#fff",
+      secondary: "#2c3e50",
+    },
+    primary: {
+      main: "#2c3e50",
+    },
+    secondary: {
+      main: "#1abc9c",
+    },
+  },
+  typography: {},
+  status: {
+    danger: orange[500],
+  },
+});
 
 function App() {
+  const callback = (
+    entries: IntersectionObserverEntry[],
+    observer: IntersectionObserver
+  ) => {
+    console.log("intersection detected");
+    console.log(entries);
+  };
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 1.0,
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+
+  const setRef = (el: HTMLElement | null) => {
+    if (el !== null) {
+      console.log("received ref");
+      console.log(el);
+      observer.observe(el);
+    }
+  };
+
+
   return (
-    <div>
-      <NavBar />
-      <Overview />
-    </div>
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        <NavBar />
+        <Section setRef={setRef} title={"Hello World"}></Section>
+        <Section setRef={setRef} title={"Next"}></Section>
+        <Section setRef={setRef} title={"WhoopWhoop"}></Section>
+      </ThemeProvider>
+    </React.Fragment>
   );
 }
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,62 +1,212 @@
-import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import logo from './logo-with-name.svg';
-import Link from '@material-ui/core/Link';
-import Button from '@material-ui/core/Button';
+import React, { useEffect, useState } from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import Button from "@material-ui/core/Button";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flexGrow: 1,
-      borderBottom: 'solid',
-      borderBottomColor: '#e0e0e0',
-      borderBottomWidth: '1px',
+      borderBottom: "solid",
+      borderBottomColor: "#e0e0e0",
+      borderBottomWidth: "1px",
     },
     flex: {
-      flexGrow: 1
+      flexGrow: 1,
     },
     linkSpacing: {
-      '& > * + *':{
-      marginLeft: theme.spacing(2)
-      }
+      "& > * + *": {
+        marginLeft: theme.spacing(2),
+      },
     },
     appBar: {
-      boxShadow: 'none',
-      backgroundColor: 'white',
-
+      boxShadow: "none",
+      backgroundColor: theme.palette.primary.main,
     },
     logo: {
-      height:'120px',
-      width: '140px',
+      height: "120px",
+      width: "140px",
     },
-  }),
+    link: {
+      "&:hover": {
+        color: theme.palette.secondary.main,
+      },
+      color: "#fff",
+    },
+    linkActive: {
+      borderRadius: "0.5rem",
+      paddingTop: "0.75rem",
+      paddingLeft: "0.5rem",
+      paddingRight: "1rem",
+      paddingBottom: "0.75rem",
+      background: theme.palette.secondary.main,
+      color: "#fff",
+    },
+  })
 );
+// link style
+// .rounded {
+//   border-radius: 0.5rem !important;
+// }
+// .px-0 {
+//   padding-right: 0 !important;
+// }
+// .px-0 {
+//   padding-left: 0 !important;
+// }
+// .py-3 {
+//   padding-top: 1rem !important;
+// }
+// .py-3 {
+//   padding-bottom: 1rem !important;
+// }
+// .px-lg-3 {
+//   padding-right: 1rem !important;
+// }
+// .navbar-brand {
+//   display: inline-block;
+//   padding-top: 0.3125rem;
+//   padding-bottom: 0.3125rem;
+//   margin-right: 1rem;
+//   font-size: 1.25rem;
+//   line-height: inherit;
+//   white-space: nowrap;
+// }
+// .navbar-brand:hover, .navbar-brand:focus {
+//   text-decoration: none;
+// }
 
-export default function DenseAppBar() {
+// @media (max-width: 991.98px) {
+//   .navbar-expand-lg > .container,
+// .navbar-expand-lg > .container-fluid,
+// .navbar-expand-lg > .container-sm,
+// .navbar-expand-lg > .container-md,
+// .navbar-expand-lg > .container-lg,
+// .navbar-expand-lg > .container-xl {
+//     padding-right: 0;
+//     padding-left: 0;
+//   }
+// }
+// @media (min-width: 992px) {
+//   .navbar-expand-lg {
+//     flex-flow: row nowrap;
+//     justify-content: flex-start;
+//   }
+//   .navbar-expand-lg .navbar-nav {
+//     flex-direction: row;
+//   }
+//   .navbar-expand-lg .navbar-nav .dropdown-menu {
+//     position: absolute;
+//   }
+//   .navbar-expand-lg .navbar-nav .nav-link {
+//     padding-right: 0.5rem;
+//     padding-left: 0.5rem;
+//   }
+//   .navbar-expand-lg > .container,
+// .navbar-expand-lg > .container-fluid,
+// .navbar-expand-lg > .container-sm,
+// .navbar-expand-lg > .container-md,
+// .navbar-expand-lg > .container-lg,
+// .navbar-expand-lg > .container-xl {
+//     flex-wrap: nowrap;
+//   }
+//   .navbar-expand-lg .navbar-nav-scroll {
+//     overflow: visible;
+//   }
+//   .navbar-expand-lg .navbar-collapse {
+//     display: flex !important;
+//     flex-basis: auto;
+//   }
+//   .navbar-expand-lg .navbar-toggler {
+//     display: none;
+//   }
+// }
+
+// .navbar {
+//   position: relative;
+//   display: flex;
+//   flex-wrap: wrap;
+//   align-items: center;
+//   justify-content: space-between;
+//   padding: 0.5rem 1rem;
+// }
+// .navbar .container,
+// .navbar .container-fluid,
+// .navbar .container-sm,
+// .navbar .container-md,
+// .navbar .container-lg,
+// .navbar .container-xl {
+//   display: flex;
+//   flex-wrap: wrap;
+//   align-items: center;
+//   justify-content: space-between;
+// }
+
+interface LinkProps {
+  href: string;
+  active: boolean;
+}
+const NavbarLink: React.FC<LinkProps> = (props) => {
   const classes = useStyles();
-  const preventDefault = (event: React.SyntheticEvent) => event.preventDefault();
+
+  const preventDefault = (event: React.SyntheticEvent) =>
+    event.preventDefault();
+
+
   return (
-    <div className={classes.root}>
-      <AppBar position="static" className={classes.appBar}>
+    <Link
+      href="#"
+      underline="none"
+      className={props.active ? classes.linkActive : classes.link}
+      onClick={preventDefault}
+    >
+       {props.children}
+    </Link>
+  );
+};
+
+interface Props {
+  setRef: (el: HTMLElement | null) => void
+}
+
+export default function Navbar(props: Props) {
+  const classes = useStyles();
+
+  const [activeLink, setActiveLink] = useState('#')
+  useEffect(() => {
+    window.onscroll = (ev: Event) => {
+      //console.log("scrolling");
+      //console.log(window.scrollX, window.scrollY);
+      if(window.scrollY > 20) {
+        //console.log('updating link')
+        setActiveLink(resultsSection);
+      }
+    };
+  }, [setActiveLink]);
+
+  const topSection = '#';
+  const aboutSection = '#about';
+  const resultsSection = '#results';
+  const teamSection = '#team';
+  const partnersSection = '#partners';
+  return (
+    <div ref={props.setRef} className={classes.root}>
+      <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
-        <img className={classes.logo} src={logo} alt="Logo" />
-      <div className={classes.flex}/>
-          <Typography variant="h6" color="textPrimary" className={classes.linkSpacing}>
-            <Link href="#" onClick={preventDefault} color="inherit">
-              Overview
-            </Link>
-            <Link href="#" onClick={preventDefault} color="inherit">
-              Team
-            </Link>
-            <Link href="#" onClick={preventDefault} color="inherit">
-              Partner
-            </Link>
-            <Link href="#" onClick={preventDefault} color="inherit">
-              Contact
-            </Link>
+          <Typography
+            variant="h6"
+            color="textPrimary"
+            className={classes.linkSpacing}
+          >
+            <NavbarLink active={false} href="#">DoDo</NavbarLink>
+            <NavbarLink active={activeLink === aboutSection} href="#about">About</NavbarLink>
+            <NavbarLink active={activeLink === resultsSection} href="#results">Results</NavbarLink>
+            <NavbarLink active={activeLink === teamSection} href="#team">Team</NavbarLink>
+            <NavbarLink active={activeLink === partnersSection} href="#partners">Partners</NavbarLink>
             <Button variant="contained">Tool Preview</Button>
           </Typography>
         </Toolbar>
