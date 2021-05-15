7b38d465a26df06a9e289cfac95ee55474ddd315
janniclas <j.n.struewer@gmail.com>
2021-05-15 23:33:48 +0200

    extracted custom intersection hook

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import Section from "../section/Section";
+import { useIntersection } from "./IntersectionHook";
 
 const fillRefsArray = (
   amount: number,
@@ -14,33 +15,7 @@ const Content = () => {
   const refs = useRef<Array<React.RefObject<HTMLElement>>>([]);
   fillRefsArray(4, refs.current);
 
-  useEffect(() => {
-    const options = {
-      root: null,
-      rootMargin: "0px",
-      threshold: 1.0,
-    };
-    const callback = (entries: IntersectionObserverEntry[]) => {
-      console.log("intersection detected");
-      console.log(entries);
-      // we can use the id of the element, which equals the href of the nav buttons,
-      // to identify the currently intersecting (active) element and thereby highlight
-      // the corresponding nav button.
-      // this should be done using mobx or some state management. just store an active
-      // section with the correct id (# represents no selection and should be on top of the page).
-    };
-    const observer = new IntersectionObserver(callback, options);
-    //observer.current = new IntersectionObserver(callback, options);
-    refs.current.forEach((ref) => {
-      if (ref.current !== null)
-        observer.observe(ref.current);
-    });
-
-    return () => {
-      console.log("disconnecting");
-      observer.disconnect();
-    };
-  }, [ refs]);
+  useIntersection(refs);
 
   return (
     <React.Fragment>
