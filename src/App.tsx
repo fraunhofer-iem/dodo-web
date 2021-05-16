28e67336f9cd18bad610df6389fe25c8cf5344c9
janniclas <j.n.struewer@gmail.com>
2021-05-16 19:38:08 +0200

    started styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import {
+  createMuiTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@material-ui/core/styles";
 import React from "react";
 import Content from "./components/content/Content";
 
 import NavBar from "./components/navbar/Navbar";
 
-const theme = createMuiTheme({
-  palette: {
-    text: {
-      primary: "#fff",
-      secondary: "#2c3e50",
+const theme = responsiveFontSizes(
+  createMuiTheme({
+    palette: {
+      text: {
+        primary: "#fff",
+        secondary: "#2c3e50",
+      },
+      primary: {
+        main: "#2c3e50",
+      },
+      secondary: {
+        main: "#1abc9c",
+      },
     },
-    primary: {
-      main: "#2c3e50",
-    },
-    secondary: {
-      main: "#1abc9c",
-    },
-  },
-  typography: {},
-});
+    typography: {},
+  })
+);
 
 export const aboutId = "about";
 export const resultsId = "results";
@@ -41,12 +47,12 @@ const navElements = [
 ];
 
 const contentElements = [
-  {element: (<div></div>), id: aboutId},
-  {element: (<div></div>), id: resultsId},
-  {element: (<div></div>), id: teamId},
-  {element: (<div></div>), id: partnersId},
+  { element: <div></div>, id: aboutId },
+  { element: <div></div>, id: resultsId },
+  { element: <div></div>, id: teamId },
+  { element: <div></div>, id: partnersId },
 ];
-
+console.log(theme.mixins.toolbar.minHeight);
 function App() {
   return (
     <React.Fragment>
--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,3 +1,4 @@
+import { Box, Container, Toolbar } from "@material-ui/core";
 import React, { useRef } from "react";
 import Section from "../section/Section";
 import { useIntersection } from "./IntersectionHook";
@@ -19,10 +20,16 @@ const Content = () => {
 
   return (
     <React.Fragment>
-      <Section href={"about"} ref={refs.current[0]}></Section>
-      <Section href={"results"} ref={refs.current[1]}></Section>
-      <Section href={"team"} ref={refs.current[2]}></Section>
-      <Section href={"partners"} ref={refs.current[3]}></Section>
+      <Toolbar />
+      <Container>
+        <Box my={2}>
+          <div>test</div>
+          <Section href={"about"} ref={refs.current[0]}></Section>
+          <Section href={"results"} ref={refs.current[1]}></Section>
+          <Section href={"team"} ref={refs.current[2]}></Section>
+          <Section href={"partners"} ref={refs.current[3]}></Section>
+        </Box>
+      </Container>
     </React.Fragment>
   );
 };
