0fced27d8c0c3708dc7974245ce01e660fed03e2
Jan-Niclas Struewer <jan-niclas.struewer@iem.fraunhofer.de>
2021-05-18 09:31:38 +0200

    first copyright version

--- src/components/footer/Copyright.tsx
+++ src/components/footer/Copyright.tsx
@@ -1,5 +1,24 @@
+import { createStyles, makeStyles, Theme } from "@material-ui/core";
 import React from "react";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      background: theme.palette.primary.dark,
+      paddingBottom: "1rem",
+      paddingTop: "1rem",
+      color: "#fff",
+    },
+  })
+);
+
 export const Copyright = () => {
-  return <div></div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>Copyright © by Fraunhofer-Gesellschaft</div>
+  );
 };
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,29 @@
+import { createStyles, makeStyles, Theme } from "@material-ui/core";
 import React from "react";
 import { Copyright } from "./Copyright";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "100%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      background: theme.palette.primary.main,
+      paddingTop: "4rem",
+    },
+  })
+);
+
 export const Footer = () => {
+  const classes = useStyles();
   return (
     <React.Fragment>
-      <Copyright />
+      <footer className={classes.root}>
+        This will be my awesome footer
+        <Copyright />
+      </footer>
     </React.Fragment>
   );
 };
